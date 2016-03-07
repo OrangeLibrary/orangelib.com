e2fff8f7ad9e5baa6ef42a5fba0dd12be7e8aa12
Ben Patterson <benthomaspatterson@gmail.com>
2016-03-06 17:05:53 -0800

    circle ci prep

---
+++ test/unit-test.js
@@ -0,0 +1,15 @@
+
+var expect = require('expect.js');
+var mocha = require('mocha');
+
+
+describe("test Suite", function(){
+
+
+  it("runs a test ",function(){
+
+    expect(2+2).to.equal(4);
+  });
+
+
+});
